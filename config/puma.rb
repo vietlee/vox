7e6c5ce600b803a10136a33bdef4011e2cb7d3df
Le Quoc Viet <quocvietlee@gmail.com>
2026-05-20 09:54:30 +0700

    Configure Puma to use Unix socket in production

--- config/puma.rb
+++ config/puma.rb
@@ -1,34 +1,18 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
-
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+if ENV["RAILS_ENV"] == "production"
+  app_dir  = File.expand_path("../..", __FILE__)
+  shared   = File.join(app_dir, "..", "..", "shared")
+  bind     "unix://#{shared}/tmp/sockets/puma.sock"
+  pidfile  "#{shared}/tmp/pids/puma.pid"
+  state_path "#{shared}/tmp/pids/puma.state"
+  stdout_redirect "#{shared}/log/puma.log", "#{shared}/log/puma.log", true
+  workers ENV.fetch("WEB_CONCURRENCY", 2)
+  preload_app!
+else
+  port ENV.fetch("PORT", 3000)
+  plugin :tmp_restart
+end
 
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
-
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
